b0825a5cc10b24dfb171e9e7e6076265c74f182d
BlueTubby_Games <93192081+BlueTubi@users.noreply.github.com>
2024-03-21 13:51:43 +0100

    V1

---
+++ Assets/BuildAssetBundle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.IO;
+
+public class BuildAssetBundle
+{
+    #if UNITY_EDITOR
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        
+        string assetBundleDirectory = "Assets/Output (DO NOT INCLUDE)";
+        if (!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+    #endif
+}
